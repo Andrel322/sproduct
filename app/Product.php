e792d91df760f460f266459140235499d771d480
Andre Luis <andrel322@gmail.com>
2017-08-10 22:54:42 -0300

    [FIX] - Ajustado comentario de entidades

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    /**
+     * Os atributos que podem ser atribuidos e salvos na tabela
+     *
+     * @var array
+     */
     protected $fillable = [
         'nome', 'descricao', 'unidadeM'
     ];
--- app/UnidadeMedida.php
+++ app/UnidadeMedida.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class UnidadeMedida extends Model
 {
+    /**
+     * Desativa a criação das colunas created_at e updated_at
+     *
+     * @var bool
+     */
     public $timestamps = false;
 }
